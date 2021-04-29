eccd918888757a16383f9acd4564f76bdd3e78ff
Liam Peachey <liamjpeachey@gmail.com>
2021-04-29 09:27:36 -0400

    Groundwork for the DB
    Completed part 4 of the tutorials

--- main_app.py
+++ main_app.py
@@ -1,9 +1,36 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LogInForm
-app = Flask(__name__)
 
+
+app = Flask(__name__)
 #This is for security related things, but we can probably remove this later since this won't be a full site.
-app.config["SECRET_KEY"] = "ce3849517d955c046577533c679b0a5e"
+app.config['SECRET_KEY'] = 'ce3849517d955c046577533c679b0a5e'
+#for the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    favs = db.relationship('Article', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Article(db.Model):
+    url = db.Column(db.String(120), primary_key=True)
+    title = db.Column(db.String(20), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f"Article('{self.title}', '{self.date}')"
 
 boxes = [
     {
