1efc5429ed4b719c3f0eb21e6674e919c2ed4eb2
Dong Huu Cao <43071789+dcaohuu18@users.noreply.github.com>
2021-05-08 23:53:29 -0400

    tag_manager can write and read tag from DB

--- app_package/routes.py
+++ app_package/routes.py
@@ -1,8 +1,12 @@
 from flask import render_template, url_for, flash, redirect
+from sqlalchemy import exc
 from app_package import app
 from app_package.forms import TagManagerForm
+from app_package import db
 from app_package.db_models import Article, Tag, ArticleTag, ArticleAction, ArticleKeyword
 from app_package.scraper import Scraper
+import sys
+
 
 boxes = [
     {
@@ -54,10 +58,27 @@ def about():
 
 @app.route('/tag-manager', methods=['GET', 'POST'])
 def tag_manager():
+    old_confirmed_tags = [tag.text for tag in Tag.query.filter_by(is_confirmed=True).all()]
+
     tag_manager_form = TagManagerForm()
-    if tag_manager_form.validate_on_submit():
-        flash('Your interest tags were successfully updated!', 'success')
-        return redirect(url_for('home')) # redirect to home
-    return render_template('tag_manager.html',
-                            title='Tag Manager',
-                            form=tag_manager_form)
+    # set the preselected to the currently confirmed tags:
+    # note that this accepts a list of tuples: (value, label)
+    tag_manager_form.tag_input.choices = [(tag, tag) for tag in old_confirmed_tags]
+
+    if tag_manager_form.submit.data: # User presses Update
+        new_confirmed_tags = tag_manager_form.tag_input.data
+
+        if len(new_confirmed_tags)>0:
+            Tag.query.filter_by(is_confirmed=True).delete() 
+            # remove all confirmed tags and add again:
+            for chosen_tag_text in new_confirmed_tags:
+                db.session.add(Tag(text=chosen_tag_text, is_confirmed=True))
+                db.session.commit()
+            
+            flash('Your interest tags were successfully updated!', 'success')
+            return redirect(url_for('home'))
+        
+        else:
+            flash('Please enter at least one tag!', 'danger')
+
+    return render_template('tag_manager.html', title='Tag Manager', form=tag_manager_form)
