daea083dc5e9b64d2f1dd78ada7f7e071aba4fa2
Dong Huu Cao <43071789+dcaohuu18@users.noreply.github.com>
2021-05-08 23:51:18 -0400

    remove dummy data and change the para of the tag select field

--- app_package/forms.py
+++ app_package/forms.py
@@ -2,12 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import SelectMultipleField, SubmitField
 from wtforms.validators import DataRequired
 
-avail_tags = [
-('world news', 'world news'),
-('sports', 'sports'),
-('covid', 'covid')
-] # (value, label)
 
 class TagManagerForm(FlaskForm):
-    tag_manager = SelectMultipleField('Interest tags', validators=[DataRequired()], choices=avail_tags)
+    tag_input = SelectMultipleField('Interest tags')
     submit = SubmitField('Update')
+
