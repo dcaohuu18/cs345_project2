74337be01e07328fa0feb8146a624b8909df755c
Dong Huu Cao <43071789+dcaohuu18@users.noreply.github.com>
2021-05-12 23:28:23 -0400

    functions to be passed to the jinja template renderer

---
+++ app_package/jinja_helper_funcs.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from urllib.parse import urlsplit, urlunsplit
+
+
+def reformat_datetime(datetime_obj, input_format='%Y-%m-%d %H:%M:%S', output_format='%H:%M %m/%d/%y'):
+    return datetime.strptime(str(datetime_obj), input_format).strftime(output_format)
+
+
+def base_url(full_url):
+    split_url = urlsplit(full_url)
+    return f"{split_url.scheme}://{split_url.netloc}"
+
+
+def est_read_time(chars_num, chars_per_min=1000):
+    return round(chars_num/1000)
